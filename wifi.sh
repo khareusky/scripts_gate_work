7aaaf089180fcf8c3ffd4b47051d305a090b1a38
root <root@gate.(none)>
2013-09-19 15:23:47 +0300

    wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #####################################
+# скрипт по открытию доступа wifi сети
+#####################################
 source /etc/gate/global.sh
 wifi="$ext3"
 wifi_addr="`ip addr show $wifi | grep inet -m 1 | awk '{print $2}' | cut -d '/' -f1`";
@@ -7,21 +9,23 @@ wifi_lan="`ip addr show $wifi | grep inet -m 1 | awk '{print $2}'`";
 
 #####################################
 # очистка правил
-while read line; do 
-	ip rule del prio "$line"
+while read prio; do 
+	ip rule del prio "$prio"
 done < <( ip rule show | grep -e '^4[0-9]:' | cut -d ':' -f1)
 
 #####################################
 # заполнение данными
-ip rule add from "$wifi_lan" table main prio 40 # from wifi to LAN
-ip rule add from "$wifi_lan" table "$ppp2" prio 41 # from wifi to internet
-ip rule add to "$wifi_lan" table main prio 42 # from all to wifi
+ip rule add from "$wifi_lan" to "$int_lan" table main prio 40 # from wifi to LAN
+ip rule add from "$wifi_lan" to "$wifi_lan" table main prio 41 # from wifi to wifi LAN
+ip rule add from "$wifi_lan" to all table "$ppp1" prio 42 # from wifi to internet
+ip rule add from all to "$wifi_lan" table main prio 43 # from all to wifi
 output "new rules:
 `ip rule ls | grep -e '^4[0-9]:'`"
 
 iptables -F INPUT_WIFI
 iptables -F FORWARD_WIFI
 iptables -t nat -F POSTROUTING_WIFI
+iptables -t mangle -F FORWARD_WIFI
 
 iptables -A INPUT_WIFI -i "$wifi" -s "$wifi_lan" -p udp --dport 53 -j ACCEPT # allow dns for wifi
 iptables -A INPUT_WIFI -i "$wifi" -s "$wifi_lan" -p udp --dport 67 -j ACCEPT # allow dhcp for wifi
