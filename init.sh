62658b4865bb7ced6d86afb3e9c4fd732bb9ff99
root <root@vpn.(none)>
2013-09-27 10:29:22 +0300

    ref

--- init.sh
+++ init.sh
@@ -1,11 +1,18 @@
-#!/bin/bash --verbose
+#!/bin/bash
 #############################################
-path=$(cd $(dirname $0) && pwd);
+path=$(cd $(dirname $0) && pwd)
+echo "path: $path"
 
 rm -f /usr/bin/global.sh
 ln -f -s $path/global.sh /usr/bin/global.sh
+ls -lsa /usr/bin/global.sh
 
 rm -f /etc/rc.local
 ln -f -s $path/rc.local /etc/rc.local
+ls -lsa /etc/rc.local
+
+if [[ ! -f /etc/init.d/openvpn ]]; then
+    echo "WARNING: you must install openvpn-client: apt-get install openvpn"
+fi
 
 #############################################
